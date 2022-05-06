ed9229c5f6c922e89c955de617be9a56fa22e9e9
veerabrave9966 <81160987+veerabrave9966@users.noreply.github.com>
2022-05-06 16:01:30 +0530

    Add files via upload

---
+++ asics.py
@@ -0,0 +1,12 @@
+print("Veera")
+
+
+a="South Korea"
+print(type(a))
+print(a)
+
+
+
+a=18
+print(type(a))
+print(a)
---
+++ asics2.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root Tk()
+root.title("Idetity Card")
+root.geometry("300x400")
+
+
+root.mailoop()
